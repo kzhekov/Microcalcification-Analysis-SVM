917edde43e587adec9060b1787afaff753036749
Unknown <kzhekov@ulb.ac.be>
2019-04-24 10:09:22 +0200

    All testing plots

--- Main.py
+++ Main.py
@@ -6,11 +6,13 @@ import matplotlib.pyplot as pyplot
 from SVMPredictingAgent import SVMPredictingAgent
 
 if __name__ == "__main__":
-    prediction_agent = SVMPredictingAgent("poly", "training_data.xlsx", "balanced_test.xlsx")
+    prediction_agent = SVMPredictingAgent("poly", "balanced_training.xlsx", "balanced_test.xlsx")
     #logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     # Testing various C parameters gives different accuracy ratios.
     list_c = []
     list_accuracy = []
-    prediction_agent.split_by_patient(3620)
+    #for i in range(2000, 4000, 100):
+    prediction_agent.split_by_patient(3420)
     #for i in range(3620, 3640, 5):
-    #    prediction_agent.k_fold_stratified_test(i, 96)
+    #prediction_agent.pca_trans_plot(3620)
+    #prediction_agent.k_fold_stratified_test(3620, 10)
--- SVMPredictingAgent.py
+++ SVMPredictingAgent.py
@@ -8,10 +8,16 @@ import pandas
 import logging
 import sys
 import matplotlib.pyplot as pyplot
+from sklearn.decomposition import PCA
+import pylab as pl
 
 
 class SVMPredictingAgent:
-
+    """
+    Class used to initialize various SVMs and use their prediction abilities to help diagnose patients based on data
+    extracted from 3D microcalcifications present in their breast tissue. Initialized with the desired SVM kernel,
+    the .xlsx file containing the training data and the .xlsx file containing the patient's data.
+    """
     def __init__(self, kernel, training_file, test_file):
         logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
         self.training_file = training_file
@@ -27,27 +33,49 @@ class SVMPredictingAgent:
         """
         Public predict method that uses the prediction samples given during the agent initialization.
         :param c_param:
-        :return:
+        :return: The accuracy of the prediction
         """
-        self.__predict(c_param, self.prediction_samples, self.prediction_classes)
+        return self.__predict(c_param, self.prediction_samples, self.prediction_classes)
 
     def split_by_patient(self, c_param):
         """
-        Splits the predictions by patient.
-        :param c_param:
-        :return:
+        Splits the prediction dataset given by the user by patient, and gives a prediction for every patient. Currently
+        gives the accuracy of the prediction for testing purposes.
+        :param c_param: The error compensation parameter.
         """
-        patient_array = [[] for i in range(int(self.patients_list_training[-1][0]))]
+        patient_array = [[] for i in range(int(self.patients_list_prediction[-1][0]) - int(self.patients_list_prediction[0][0])+1)]
+        patient_class = [[] for i in range(int(self.patients_list_prediction[-1][0]) - int(self.patients_list_prediction[0][0])+1)]
         print("Number of patients: ", patient_array.__len__())
-        for i in range(len(self.patients_list_training)):
-            patient_array[int(self.patients_list_training[i][0])-1].append(self.patients_list_training[i][1:])
+        for i in range(len(self.patients_list_prediction)):
+            to_add = int(self.patients_list_prediction[i][0]) - int(self.patients_list_prediction[0][0])
+            patient_array[to_add].append(self.patients_list_prediction[i][1:])
+            patient_class[to_add].append(self.prediction_classes[i])
         patient_array = numpy.array(patient_array)
+        patient_class = numpy.array(patient_class)
         counter = 0
+        predictions_accuracy = []
         for patient_data in patient_array:
             patient_data = numpy.array(patient_data)
             counter += 1
-            print("Current patient:", counter, "has the following number of characteristics:", patient_data.shape)
-            print("Current patient:", counter, "predicted with accuracy:", self.__predict(c_param, patient_data, [0 if i < 47 else 1 for i in range(len(patient_data))]))
+            #print("Current patient data:", counter, "has the following shape:", patient_data.shape)
+            #print("Current patient classes:", counter, "has the following values:", patient_class[counter-1])
+            current_prediction = self.__predict(c_param, patient_data,
+                                                [patient_class[counter-1][0] for i in range(len(patient_data))])
+            predictions_accuracy.append(current_prediction)
+            print("Current patient:", counter, "predicted with accuracy:", current_prediction)
+
+        predictions_accuracy_mean = [numpy.mean(predictions_accuracy)] * len(predictions_accuracy)
+        print(predictions_accuracy_mean[0])
+        fig, ax = pyplot.subplots()
+        ax.plot(range(counter), predictions_accuracy, label="Patient Predictions Accuracy", marker='o')
+        ax.plot(range(counter), predictions_accuracy_mean, label="Mean Prediction Accuracy", linestyle='--')
+        ax.legend(loc='upper right')
+        ax.set(xlabel='Fold', ylabel='Accuracy',
+               title=('Accuracy test per patient with 3-' + self.kernel + ' kernel, C=' + str(c_param)))
+        ax.grid()
+        ax.text(0, 0.7, ("Mean = " + str(predictions_accuracy_mean[0])))
+        print("Saving file :", ('Accuracy test per patient with ' + self.kernel + ' kernel, C=' + str(c_param)))
+        fig.savefig(('Accuracy test per patient with 3-' + self.kernel + ' kernel, C=' + str(c_param)))
 
     def __predict(self, c_param, prediction_samples, prediction_classes):
         """
@@ -64,7 +92,6 @@ class SVMPredictingAgent:
             self.micro_svm = self.init_linear_svm(c_param)
 
         predictions = self.micro_svm.predict(prediction_samples)
-        #print(predictions)
         accuracy = self.compute_accuracy(prediction_classes, predictions)
 
         logging.debug("End of predictions.")
@@ -85,7 +112,7 @@ class SVMPredictingAgent:
         logging.debug("End of SVM initialization.")
         return micro_svm
 
-    def init_poly_svm(self, c_param, dgr=2):
+    def init_poly_svm(self, c_param, dgr=3):
         """
         Initializes and trains an SVM to predict whether described microcalcifications in input data are benign or
         malicious.
@@ -115,9 +142,83 @@ class SVMPredictingAgent:
         data_samples = data.values[:, 1:-1]  # Remove first column (patient number) and last column (result)
         data_labels = list(data.columns)  # Labels of all columns
         data_samples = self.preprocess_data(data_samples)
+        patient_numbers[:, 1:] = data_samples
 
         logging.debug("End of parsing.")
         return data_samples, data_classes, data_labels, patient_numbers
+
+    def pca_trans_plot(self, c_param):
+
+        def make_meshgrid(x, y, h=.02):
+            """
+            Create a mesh of points to plot in.
+            :param x: The data to base x-axis meshgrid on.
+            :param y: The data to base y-axis meshgrid on.
+            :param h: The stepsize for meshgrid (optional).
+            :return : (xx, yy) ndarrays
+            """
+            x_min, x_max = x.min() - 1, x.max() + 1
+            y_min, y_max = y.min() - 1, y.max() + 1
+            xx, yy = numpy.meshgrid(numpy.arange(x_min, x_max, h),
+                                 numpy.arange(y_min, y_max, h))
+            return xx, yy
+
+        def plot_contours(ax, clf, xx, yy, **params):
+            """
+            Plot the decision boundaries for a classifier.
+
+            Parameters
+            ----------
+            :param ax: matplotlib axes object
+            :param clf: a classifier
+            :param xx: meshgrid ndarray
+            :param yy: meshgrid ndarray
+            :param params: dictionary of parameters to pass to the contourf function (optional).
+            :return : out, the response of the contourf function.
+            """
+            Z = clf.predict(numpy.c_[xx.ravel(), yy.ravel()])
+            Z = Z.reshape(xx.shape)
+            out = ax.contourf(xx, yy, Z, **params)
+            return out
+
+        # Use feature extraction to get the features that influence the most the distribution of the dataset.
+        logging.debug("Starting PCA feature extraction.")
+        pca = PCA(n_components=2).fit(self.training_samples)
+        X = pca.transform(self.training_samples)
+        y = self.training_classes
+        logging.debug("End of PCA feature extraction.")
+
+        # We create an instance of SVM and fit out data. No scaling needed for the support vectors plot
+        logging.debug("Initialising SVM with 2D PCA matrix.")
+        models = (svm.SVC(kernel='linear', C=c_param),
+                  svm.SVC(kernel='poly', degree=2, C=c_param))
+        models = (clf.fit(X, y) for clf in models)
+        logging.debug("End of SVM initialization with 2D PCA matrix.")
+
+        # Title for the plots
+        titles = ('SVC with linear kernel',
+                  'SVC with 2-polynomial kernel')
+
+        # Set-up 2x1 grid for plotting linear and 2-poly kernel.
+        fig, sub = pyplot.subplots(2, 1)
+        pyplot.subplots_adjust(wspace=0.8, hspace=0.8)
+
+        X0, X1 = X[:, 0], X[:, 1]
+        xx, yy = make_meshgrid(X0, X1)
+
+        for clf, title, ax in zip(models, titles, sub.flatten()):
+            plot_contours(ax, clf, xx, yy,
+                          cmap=pyplot.cm.coolwarm, alpha=0.8)
+            ax.scatter(X0, X1, c=y, cmap=pyplot.cm.coolwarm, s=20, edgecolors='k')
+            ax.set_xlim(xx.min(), xx.max())
+            ax.set_ylim(yy.min(), yy.max())
+            ax.set_xlabel('PCA Parameter 1')
+            ax.set_ylabel('PCA Parameter 2')
+            ax.set_xticks(())
+            ax.set_yticks(())
+            ax.set_title(title)
+
+        pyplot.show()
 
     def k_fold_stratified_test(self, c_param, k):
         """
@@ -151,9 +252,9 @@ class SVMPredictingAgent:
         list_c = []
         list_accuracy = []
 
-        for i in range(5):
+        for i in range(k):
             if self.kernel == "poly":
-                self.micro_svm = svm.SVC(kernel="poly", degree=2, gamma="auto", C=c_param)
+                self.micro_svm = svm.SVC(kernel="poly", degree=3, gamma="auto", C=c_param)
                 self.micro_svm.fit(X_train[i], y_train[i])
             elif self.kernel == "linear":
                 self.micro_svm = svm.SVC(kernel="linear", gamma="auto", C=c_param)
@@ -164,16 +265,16 @@ class SVMPredictingAgent:
             list_accuracy.append(accuracy)
 
         k_fold_mean = [numpy.mean(list_accuracy)]*len(list_c)
-        print(k_fold_mean)
+        print(k_fold_mean[0])
         fig, ax = pyplot.subplots()
         ax.plot(list_c, list_accuracy, label="Data", marker='o')
         ax.plot(list_c, k_fold_mean, label="Mean", linestyle='--')
         ax.legend(loc='upper right')
         ax.set(xlabel='Fold', ylabel='Accuracy',
-               title=('Accuracy for K-fold test with 2-poly kernel and PN, C=' + str(c_param)))
+               title=('Accuracy for K-fold test with 3-poly kernel and PN, C=' + str(c_param)))
         ax.grid()
         ax.text(0, 0.7, ("Mean = "+str(k_fold_mean[0])))
-        fig.savefig(('10 K-fold test with 2-poly kernel and PN, C=' + str(c_param)))
+        fig.savefig((str(k) + '-fold test with 3-poly kernel and PN, C=' + str(c_param)))
         #pyplot.show()
 
     @staticmethod
